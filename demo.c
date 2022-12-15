2883e0d91ee7f47a7b76d485a38800613f331359
patbc <culleyp@oregonstate.edu>
2022-12-14 19:56:45 -0600

    added table and test data

--- demo.c
+++ demo.c
@@ -4,7 +4,7 @@
 
 /*Basic MySQL functions:
 * ----------------------
-1. mysql_init()				Allocates or initialises a MYSQL object used by 
+1. mysql_init()				Allocates or initialises a MYSQL object used by mysql_real_connect
 2. mysql_get_client_info	Returns MySQL version number 
 3. mysql_error()							
 4. mysql_query() 
@@ -27,32 +27,38 @@ int main(int argc, char** argv) {
 
 	// 2 Steps to create DATABASE: 
 	// ---------------------------
+
 	// ANALYZE THIS! what does it do? How does it do it? 
 	// This is required to create a DATABASE (1)
+
 	if (mysql_real_connect(con, "localhost", "root", "Gh33c@t86",
-		NULL, 0, NULL, 0) == NULL)
+		"maindb", 0, NULL, 0) == NULL)
 	{
 		fprintf(stderr, "%s\n", mysql_error(con));
-		mysql_close(con);
-		exit(1);
+		//mysql_close(con);
+		//exit(1);
 	}
+
+
 	// Verify if db can be created 
 	// This is required to create DATABASE (2) 
+
 	if (mysql_query(con, "CREATE DATABASE maindb")) {
 
 		printf(stderr, "%s\n", mysql_error(con));
-		mysql_close(con);
+		//mysql_close(con);
 	};
 
-	// Print all databases: 
-	MYSQL_RES* result = mysql_list_dbs(con, NULL);			// Returns a result set of databases 
-
-
-	/* Steps to create and populate TABLE
-	* -----------------------------------
-	* 
-	*/ 
-
-	mysql_close(con); 
-	return 0; 
+	//mysql_real_connect(con, "localhost", "user12", "34klq*", "maindb", 0, NULL, 0);
+ 
+	mysql_query(con, "DROP TABLE IF EXISTS items"); 
+	mysql_query(con, "CREATE TABLE items(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255))");
+	mysql_query(con, "INSERT INTO items VALUES(1, 'eat eggs'\n)");
+	mysql_query(con, "INSERT INTO items VALUES(2, 'eat steak'\n)");
+	mysql_query(con, "INSERT INTO items VALUES(3, 'eat chcken')");
+	mysql_query(con, "INSERT INTO items VALUES(4, 'eat broccolli')");
+	mysql_query(con, "INSERT INTO items VALUES(5, 'eat salad')");
+	mysql_query(con, "INSERT INTO items VALUES(6, 'eat rice & beans')");
+	mysql_close(con);
+	exit(0); 
 }
