42ca67625ce4a6a26336f000ca516453803db15f
patbc <culleyp@oregonstate.edu>
2022-12-28 19:11:17 -0600

    updated insert function

--- demo.c
+++ demo.c
@@ -2,38 +2,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Displays all db rows -------------------------------------------------------------------
-void retreive_rows(MYSQL* con) {
-	mysql_query(con, "SELECT * FROM tasks");
-	MYSQL_RES* res = mysql_store_result(con);		// iterate over stored records 
-	MYSQL_ROW row;
+void create_table(MYSQL* con) {
+	char table_name[50];
 
-	// Iterate over each row, fetching id & task 
-	while ((row = mysql_fetch_row(res))) {
-		printf("%s | ", row[0]);
-		printf("%s\n", row[1]);
-	};
-	printf("\n"); 
-	mysql_free_result(res);
 };
 
-// Inserts new records --------------------------------------------------------------------
-void create_task(MYSQL* con) {
-	char task[50]; 
-	char buffer[50];
 
-	printf("Enter task to add	: ");
-	fgets(task, 50, stdin);
+// Retrieve tables ----------------------------------------------------------------------------
+void retreive_tables(MYSQL* con) {
+	mysql_query(con, "SHOW TABLES"); 
+	MYSQL_RES* res = mysql_store_result(con);
+	MYSQL_ROW row; 
+	int count; 
+
+	printf("Tables\n"); 
+	printf("------\n"); 
+
+	count = 1; 
+	while ((row = mysql_fetch_row(res))) {
+		printf("%d | ", count);
+		printf("%s\n", row[0]);
+		count++; 
+	};
+	printf("\n");
+	mysql_free_result(res);
+
+};
+
+// Displays all table rows --------------------------------------------------------------------
+void retreive_rows(MYSQL* con) {
+	mysql_query(con, "SELECT * FROM diabetes");
+	MYSQL_RES* res = mysql_store_result(con);		// Used to iterate over all stored records 
+	MYSQL_ROW row;
+
+	// Iterate over rows, printing id & task 
+	while ((row = mysql_fetch_row(res))) {
+		printf("%s | ", row[0]);
+	};
+	printf("\n"); 
+	mysql_free_result(res);						   // free prior to returing to main 
+};
+
+// Inserts new record -------------------------------------------------------------------------
+void create_task(MYSQL* con) {
+	char meal[50]; 
+	char buffer[50];
+	char calories[5]; 
+	int num;
+
+	printf("Enter meal: ");
+	fgets(meal, 50, stdin);
+	printf("Enter calories: ");
+	fgets(calories, 5,stdin);
+	num = atoi(calories); 
+
 
 	// Replace newline with null char to prevent empty rows
-	task[strlen(task) - 1] = '\0';			
-   	sprintf(buffer, "INSERT INTO tasks(task) VALUES('%s')", task); 
-	mysql_query(con, &buffer); 
+	meal[strlen(meal) - 1] = '\0';		
+   	sprintf(buffer, "INSERT INTO diabetes(meal, calories) VALUES('%s', %d)", meal, num); 
+	printf("%s", buffer); 
+
+	mysql_query(con, buffer); 
 }; 
 
-// Deletes records from DB -----------------------------------------------------------------
+// Deletes record from table ------------------------------------------------------------------
 void delete_record(MYSQL* con) {
-	char id[5]; 
+	char id[5];  
 	char buffer[50]; 
 	int num; 
 
@@ -41,69 +75,75 @@ void delete_record(MYSQL* con) {
 	fgets(id, 5, stdin);		// To convert char to int
 	num = atoi(id); 
 
-	sprintf(buffer, "DELETE FROM tasks WHERE id=(%d)", num); 
+	sprintf(buffer, "DELETE FROM diabetes WHERE id=(%d)", num); 
 	mysql_query(con, &buffer); 
 }; 
-
+ 
 int main(int argc, char** argv) {
 	int count; 
 
-	// Holds the server & db connection 
+	// Holds server & db connection 
 	MYSQL* con = mysql_init(NULL);
 
 	if (con == NULL) {
 		fprintf(stderr, "%s\n", mysql_error(con));
 		exit(1);
 	}
-	if (mysql_real_connect(con, "localhost", "root", "Gh33c@t86",
+	if (mysql_real_connect(con, "localhost", "user", "password",
 		NULL, 0, NULL, 0) == NULL) {
 			fprintf(stderr, "%s\n", mysql_error(con));
 			mysql_close(con);
 			exit(1);
 	}
-	mysql_query(con, "CREATE DATABASE aarrgghh");		
+	mysql_query(con, "CREATE DATABASE newDB");		
 	
-	// Use prior, to initialize a new connection with the newly created database.
+	// Close prior to initializing a new connection with the newly created database
 	mysql_close(con);							 
 
 	con = mysql_init(NULL);
-	mysql_real_connect(con, "localhost", "root", "Gh33c@t86", "aarrgghh", 0, NULL, 0);		// connection containing new database. 
+	mysql_real_connect(con, "localhost", "user", "password", "newDB", 0, NULL, 0);			// connection containing new database. 
 
 	//Create Table------------------------------------------------------------------------------
-	mysql_query(con, "CREATE TABLE tasks(id INT PRIMARY KEY AUTO_INCREMENT, task VARCHAR(255))");
+	mysql_query(con, "CREATE TABLE diabetes(id INT PRIMARY KEY AUTO_INCREMENT, meal VARCHAR(255), cals INT)");
 
-	// Gather input-----------------------------------------------------------------------------
-	printf("                                      << WELCOME TO TASK TRACKER >>\n\n");
+	// Instructions -----------------------------------------------------------------------------
+	printf("                                      << WELCOME TO DIABEATES ANALYZER >>\n\n");
 	printf("                            Enter number from one of the following options below: \n");
-	printf("                                            1. View tasks \n"); 
-	printf("                                            2. Create task \n");
-	printf("                                            3. Delete task \n");
-	printf("                                            4. Exit program \n\n");
+	printf("                                            1. View all entries \n"); 
+	printf("                                            2. Create meal entry \n");
+	printf("                                            3. Delete entry \n");
+	printf("                                            4. Exit program \n");
+	printf("                                            5. View all tables\n\n");
 
+	// Gather User Input -----------------------------------------------------------------------
 	char choice[10];
 	int num, ret;
 
 	while (1) {
 		printf("Enter number: ");
 		fgets(choice, 10, stdin);
+		printf("\n");
 		num = atoi(choice);
 
 		switch (num) {
-		case 1:
-			retreive_rows(con);
-			break;
-		case 2:
-			create_task(con);
-			break;
-		case 3:
-			delete_record(con);
-			break;
-		case 4:
-			return 0;
+			case 1:
+				retreive_rows(con);
+				break;
+			case 2:
+				create_task(con);
+				break;
+			case 3:
+				delete_record(con);
+				break;
+			case 4:
+				return 0;
+			case 5: 
+				retreive_tables(con);
+				break; 
+			case 6: 
+				create_table(con); 
 		}; 
 	}; 
-
-	exit(1);
 	mysql_close(con);
 	exit(0); 
 }
